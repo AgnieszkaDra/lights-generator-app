b55f10c0206b7cb6443ac8926d777d66c7739f45
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-12-14 20:20:24 +0100

    feat:create-counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+// eslint-disable-next-line no-unused-vars
+import { Counter } from "./components/Counter";
 
 function App() {
-  return <div>Lights generator</div>;
+  return (
+    <div className="wrapper">
+      <nav className="nav">
+        <p className="counter">0 bulbs</p>
+        <Counter text={'rows'} count={0}/>
+        <Counter text={'columns'} count={0}/>
+      </nav>
+    </div>
+  )
 }
 
 export default App;
