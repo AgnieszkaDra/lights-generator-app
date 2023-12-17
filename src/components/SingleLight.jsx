309e77aab9e67ef1790c3e38a73691a93a86d962
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-12-17 14:52:53 +0100

    randomize colors

--- src/components/SingleLight.jsx
+++ src/components/SingleLight.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import LightBlue from '../../public/images/light-blue.svg'
+import LightOrange from '../../public/images/light-orange.svg'
+import LightYellow from '../../public/images/light-yellow.svg'
+import LightPink from '../../public/images/light-pink.svg'
+import LightRed from '../../public/images/light-red.svg'
 import LightTurnedOff from '../../public/images/light-turned-off.svg'
+
+
+const lights = [LightBlue, LightOrange, LightYellow, LightPink, LightRed]
 
 export const SingleLight = () => {
     const [isOn, setOn] = useState(true)
-    const imageSrc = isOn ? LightBlue : LightTurnedOff
+    const getRandomIndex = () => {
+        return (
+            Math.floor(Math.random() * lights.length)
+        )
+    }
+    const randomIndex = getRandomIndex()
+    const imageSrc = isOn ? lights[randomIndex] : LightTurnedOff
     const handleToggle = () => {
         setOn(!isOn)
     }
