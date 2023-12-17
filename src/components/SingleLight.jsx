fdaaf10b94e59433f83a7b943b49f8db85d17d97
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-12-17 15:15:09 +0100

    add useEfect hook

--- src/components/SingleLight.jsx
+++ src/components/SingleLight.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LightBlue from '../../public/images/light-blue.svg'
 import LightOrange from '../../public/images/light-orange.svg'
 import LightYellow from '../../public/images/light-yellow.svg'
@@ -11,13 +11,19 @@ const lights = [LightBlue, LightOrange, LightYellow, LightPink, LightRed]
 
 export const SingleLight = () => {
     const [isOn, setOn] = useState(true)
+    const [lightColor, setLightColor] = useState(LightBlue)
     const getRandomIndex = () => {
         return (
             Math.floor(Math.random() * lights.length)
         )
     }
-    const randomIndex = getRandomIndex()
-    const imageSrc = isOn ? lights[randomIndex] : LightTurnedOff
+    // const randomIndex = getRandomIndex()
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * lights.length);
+        setLightColor(lights[randomIndex]);
+    }, []); // Empty dependency array ensures this runs once on mount
+    // const imageSrc = isOn ? lights[randomIndex] : LightTurnedOff
+    const imageSrc = isOn ? lightColor : LightTurnedOff
     const handleToggle = () => {
         setOn(!isOn)
     }
